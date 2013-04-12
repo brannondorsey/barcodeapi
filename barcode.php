006818099bdcbe7d04e1149a64e03bbfd356f5e6
Brannon Dorsey <brannon@brannondorsey.com>
2013-04-12 00:48:03 -0500

    updated barcode ap.php to accept a scale parameter in the get request from processing

--- barcode.php
+++ barcode.php
@@ -5,10 +5,10 @@
     $bar = new BARCODE();
     $upc = $_GET['code'];
     $height = $_GET['height'];
-    $width = $_GET['width'];
+    $scale = $_GET['scale'];
     $color = $_GET['color'];
     // Simple use with only the four mandatory parameters. File will be saved in scripts folder
-    $bar->BarCode_save("UPC-A", $upc, $upc, "images/", "png", $height, $width, "#FFFFFF", "#".$color);
+    $bar->BarCode_save("UPC-A", $upc, $upc, "images/", "png", $height, $scale, "#FFFFFF", "#".$color);
     echo "<img src='".$_SESSION["_CREATED_FILE_"]."' />";
 ?>
 </html>
